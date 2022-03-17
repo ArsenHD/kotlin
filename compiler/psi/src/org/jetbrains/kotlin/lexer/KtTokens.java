f554d20ce7c92a2467a34bbb387e66b4d748ece6
Arsen Nagdalian <nagoarsen@mail.ru>
2022-03-17 12:13:09 +0300

    [PSI] Add soft keyword 'static'

--- compiler/psi/src/org/jetbrains/kotlin/lexer/KtTokens.java
+++ compiler/psi/src/org/jetbrains/kotlin/lexer/KtTokens.java
@@ -175,6 +175,7 @@ public interface KtTokens {
     int IMPL_KEYWORD_Id = 146;
     int EXPECT_KEYWORD_Id = 147;
     int ACTUAL_KEYWORD_Id = 148;
+    int STATIC_KEYWORD_Id = 149;
 
     KtToken EOF   = new KtToken("EOF", EOF_Id);
 
@@ -322,6 +323,8 @@ public interface KtTokens {
     KtKeywordToken DYNAMIC_KEYWORD   = KtKeywordToken.softKeyword("dynamic", DYNAMIC_KEYWORD_Id);
     KtModifierKeywordToken COMPANION_KEYWORD = KtModifierKeywordToken.softKeywordModifier("companion", COMPANION_KEYWORD_Id);
     KtModifierKeywordToken SEALED_KEYWORD    = KtModifierKeywordToken.softKeywordModifier("sealed", SEALED_KEYWORD_Id);
+    KtModifierKeywordToken STATIC_KEYWORD      = KtModifierKeywordToken.softKeywordModifier("static", STATIC_KEYWORD_Id);
+
 
     KtModifierKeywordToken DEFAULT_VISIBILITY_KEYWORD = PUBLIC_KEYWORD;
 
@@ -373,7 +376,7 @@ public interface KtTokens {
                                              DATA_KEYWORD, INLINE_KEYWORD, NOINLINE_KEYWORD, TAILREC_KEYWORD, EXTERNAL_KEYWORD,
                                              ANNOTATION_KEYWORD, CROSSINLINE_KEYWORD, CONST_KEYWORD, OPERATOR_KEYWORD, INFIX_KEYWORD,
                                              SUSPEND_KEYWORD, HEADER_KEYWORD, IMPL_KEYWORD, EXPECT_KEYWORD, ACTUAL_KEYWORD,
-                                             VALUE_KEYWORD, CONTEXT_KEYWORD
+                                             VALUE_KEYWORD, CONTEXT_KEYWORD, STATIC_KEYWORD
     );
 
     /*
@@ -388,7 +391,7 @@ public interface KtTokens {
                     REIFIED_KEYWORD, COMPANION_KEYWORD, SEALED_KEYWORD, LATEINIT_KEYWORD,
                     DATA_KEYWORD, INLINE_KEYWORD, NOINLINE_KEYWORD, TAILREC_KEYWORD, EXTERNAL_KEYWORD, ANNOTATION_KEYWORD, CROSSINLINE_KEYWORD,
                     CONST_KEYWORD, OPERATOR_KEYWORD, INFIX_KEYWORD, SUSPEND_KEYWORD,
-                    HEADER_KEYWORD, IMPL_KEYWORD, EXPECT_KEYWORD, ACTUAL_KEYWORD, FUN_KEYWORD, VALUE_KEYWORD
+                    HEADER_KEYWORD, IMPL_KEYWORD, EXPECT_KEYWORD, ACTUAL_KEYWORD, FUN_KEYWORD, VALUE_KEYWORD, STATIC_KEYWORD
             };
 
     TokenSet MODIFIER_KEYWORDS = TokenSet.create(MODIFIER_KEYWORDS_ARRAY);
